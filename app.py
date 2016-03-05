5a184ffad8d2f8948dcf3f0c4c4b5cfe37f38eb2
Alan Plotko <alan.plotko@gmail.com>
2016-03-05 16:53:27 -0500

    Refactor room joining and add new requirement

--- app.py
+++ app.py
@@ -49,9 +49,9 @@ def background_thread():
     while True:
         time.sleep(10)
         count += 1
-        socketio.emit('my response',
+        '''socketio.emit('my response',
                       {'data': 'Server generated event', 'count': count},
-                      namespace='/test')
+                      namespace='/test')'''
 @app.route('/')
 def index():
     global thread
@@ -70,18 +70,11 @@ def rules():
         thread.start()
     return render_template('rules.html')
 
-#this cannot be called def rooms(name)
-@app.route('/room/<name>/<id>')
-def roomhtml(name, idval):
-    return render_template('room.html', name=name, socketid=idval)
-
-
 @socketio.on('my event', namespace='/test')
 def test_message(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my response',
          {'data': message['data'], 'count': session['receive_count']})
-
 
 @socketio.on('my broadcast event', namespace='/test')
 def test_broadcast_message(message):
@@ -90,13 +83,14 @@ def test_broadcast_message(message):
          {'data': message['data'], 'count': session['receive_count']},
          broadcast=True)
 
-
 @socketio.on('join', namespace='/test')
 def join(message):
+    session['username'] = message['username']
     join_room(message['room'])
+    socketio.send(session['username'] + ' has entered the room.', room=message['room'])
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my response',
-         {'data': 'In rooms: ' + ', '.join(rooms()),
+         {'data': 'Welcome to the channel: ' + message['room'],
           'count': session['receive_count']})
 
 @socketio.on('leave', namespace='/test')
@@ -135,7 +129,7 @@ def disconnect_request():
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
-    emit('my response', {'data': 'Connected', 'count': 0})
+    emit('my response', {'data': 'Establishing connection...', 'count': 0})
 
 
 @socketio.on('disconnect', namespace='/test')
