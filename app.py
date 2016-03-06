a7b22fb3847b3544749501b5141240ef1823214d
Miguel L <wzzch@naver.com>
2016-03-06 13:49:31 -0500

    bergie

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ def index():
         thread2 = Thread(target=input_thread)
         thread2.daemon = True
         thread2.start()
-    return render_template('index.html')
+    return render_template('index.html', port=port)
 
 @app.route('/rules/')
 def rules():
